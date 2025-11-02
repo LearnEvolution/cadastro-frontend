e8e3ba68eff9ad30e1f0e236ac24d4f31b03c9d8
psh.evolution <psh.evolution@gmail.com>
2025-11-02 04:28:32 -0300

    primeiro commit frontend react

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,53 @@
+import { useEffect, useSate } from 'react'
+import './style.css'
+import Lixeira from '../../assets/lixeira.png'
+import api from '../../services/api'
+   
+   
+    function Home() {
+    const [users, setUsers] = useState([])    
+
+	async function getUsers(){
+          try{
+	   const usersFromApi = await api.get('/usuarios')
+       setUsers(usersFromApi.data)		
+        } catch (error) {
+             console.error('Erro ao buscar usuarios:', error)
+        }
+      }
+
+      useEffect(() => {
+	getUsers()
+	
+      }, [])
+
+
+  return(
+         <div className='container'>
+            <form>
+		<h1>Cadastro de Usuarios</h1>
+		<input placeholder='Nome' name='nome' type='text'/>
+		<input placeholder='Idade' name='idade' type='number'/>
+		<input placeholder='E-mail' name='email' type='text'/>
+
+		<button type='button'>cadastrar</button>
+	    </form>
+	  
+            { users.map(user => (
+
+                <div key={user.id} className='card'>
+                     <div>
+                        <p>Nome: <span>{user.name}</span></p>
+                        <p>Idade: <span>{user.age} </span></p>
+                        <p>Email: <span>{user.email}</span></p>
+                     </div>
+                        <button>
+                            <img src={Lixeira}/>
+                        </button>
+                </div>
+             ))}
+           </div>
+   )
+}
+
+export default Home
