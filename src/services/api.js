2a6cb19e538ad1deeb06341b361bd56c9de03903
psh.evolution <psh.evolution@gmail.com>
2025-11-07 17:47:30 -0300

    atualizar URL de API e LIMPAR arquivos invalidos

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-	baseURL: 'http://localhost:3000'
+  baseURL: 'https://cadastro-api-1.onrender.com' // <-- URL pública da sua API no Render
 })
-
 
 export default api
