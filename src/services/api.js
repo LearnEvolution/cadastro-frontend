c155f1b0fde63dff0748c686aaf2856bcd10724c
psh.evolution <psh.evolution@gmail.com>
2025-11-14 09:26:21 -0300

    troque o endereço para atual

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://cadastro-api-jctq.onrender.com'
+  baseURL: 'https://cadastro-api-1.onrender.com'
 });
 
 export default api;
